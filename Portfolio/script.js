4f09a49c5d6c95d20a9e71a12f9fedf676fdf37e
fationhyso <hiusofation123@gmail.com>
2023-03-12 14:36:37 +0200

    Slider

--- Portfolio/script.js
+++ Portfolio/script.js
@@ -1,21 +1,32 @@
-var swiper = new Swiper('.Design-slider', {
-    effect: 'coverflow',
-    grabCursor: true,
-    centeredSlides: true,
-    loop: true,
-    slidesPerView: 'auto',
-    coverflowEffect: {
-      rotate: 0,
-      stretch: 0,
-      depth: 100,
-      modifier: 2.5,
-    },
-    pagination: {
-      el: '.swiper-pagination',
-      clickable: true,
-    },
-    navigation: {
-      nextEl: '.swiper-button-next',
-      prevEl: '.swiper-button-prev',
-    }
-  });
+const swiper = new Swiper('.swiper', {
+  // Optional parameters
+  loop: true,
+  effect: 'coverflow',
+  grabCursor: true,
+  centeredSlides: true,
+  loop: true,
+  slidesPerView: 'auto',
+  coverflowEffect: {
+    rotate: 2,
+    stretch: 0,
+    depth: 550,
+    modifier: 3,
+    slideShadows: false,
+  },
+
+  // If we need pagination
+  pagination: {
+    el: '.swiper-pagination',
+  },
+
+  // Navigation arrows
+  navigation: {
+    nextEl: '.swiper-button-next',
+    prevEl: '.swiper-button-prev',
+  },
+
+  // And if we need scrollbar
+  scrollbar: {
+    el: '.swiper-scrollbar',
+  },
+});
