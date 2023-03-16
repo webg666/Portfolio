4be7ec107557ef9014792a6f43c64dd097743774
fationhyso <hiusofation123@gmail.com>
2023-03-16 13:54:04 +0200

    Design Update

--- Portfolio/script.js
+++ Portfolio/script.js
@@ -43,3 +43,47 @@ const observer = new IntersectionObserver((entries) =>{
 
 const hiddenElements = document.querySelectorAll('.hidden');
 hiddenElements.forEach((el) => observer.observe(el));
+
+
+///Video animation
+
+const Home =document.querySelector('.Home');
+const video = Home.querySelector('video');
+const text = Home.querySelector('h1');
+//End dection
+const section =document.querySelector('section');
+const text1 = section.querySelector('h1');
+
+//scroll magic
+const controller = new ScrollMagic.Controller();
+
+
+///scenes
+const scene = new ScrollMagic.Scene({
+    duration :350,
+    triggerElemnt:Home,
+    triggerHook: 0,
+
+})
+.addIndicators()
+.setPin(Home)
+.addTo(controller);
+
+
+//video animetion
+
+let accelamout = 0.1;
+let scrollpos = 0;
+let delay =0 ;
+
+scene.on('update', e =>{
+    scrollpos = e.scrollPos / 100;
+
+}) 
+
+setInterval(() =>{
+    delay += (scrollpos-delay) * accelamout;
+    console.log(scrollpos, delay);
+
+    video.currentTime = delay;
+}, 200);
